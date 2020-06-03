45aba466c3824be66ee9e361f8bfb8f1c1d3527c
Jason Ours <jasonours@outlook.com>
2020-06-03 12:21:04 -0500

    working project

--- src/App.test.js
+++ src/App.test.js
@@ -1,33 +1,25 @@
 import React from 'react';
-import { render, fireEvent, wait, queryAllByTestId } from '@testing-library/react';
-import { fetchMissions as mockFetchMissions } from './api/fetchMissions';
-import { missionsFixture } from './components/MissionsList.test' 
+import {render, fireEvent, waitFor} from '@testing-library/react';
+import {fetchMissions as mockFetchMissions} from './api/fetchMissions';
 import App from './App';
+
+import {missionsFixture} from './components/MissionsList.test';
 import { act } from 'react-dom/test-utils';
 
-jest.mock('./api/fetchMissions');
-console.log(mockFetchMissions)
+jest.mock("./api/fetchMissions");
+// console.log("bk: App.test.js: mockFetchMissions: ", mockFetchMissions);
 
-test("App fetches and renders missions data", async ()=> {
-    mockFetchMissions.mockResolvedValueOnce(missionsFixture);
+test("App fetches and renders missions data", async ()=>{
+  // expect(false).toBe(false);
+  mockFetchMissions.mockResolvedValueOnce({data: missionsFixture});
 
-    const {getByText, queryAllByTestId} = render(<App />);
+  const {getByText, queryAllByTestId} = render(<App />);
 
-    const button = getByText(/get data/i);
-  
-    await waitFor(() => {
-        
-    });
-    fireEvent.click(button);
+  const button = getByText(/get data/i);
+  fireEvent.click(button);
+  getByText(/we are fetching data/i);
 
-    getByText(/we are fetching data/i);
-
-    
-    const missionElems = await queryAllByTestId('mission')
-    
-    expect (missionElems).toHaveLength(2);
-
-    // await wait();
-    // expect(queryAllByTestId('mission')).toHaveLength(0);
-    
+  await waitFor(()=>{
+    expect(queryAllByTestId("mission")).toHaveLength(2);
+  });
 })
